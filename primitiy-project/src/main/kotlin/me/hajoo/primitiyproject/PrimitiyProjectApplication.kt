58f82f1bc8ecf625f1e53d8bceff5de14e85414d
joohyun.ha <mylifeforcoding@gmail.com>
2023-04-17 23:21:20 +0900

    init: primity project

---
+++ primitiy-project/src/main/kotlin/me/hajoo/primitiyproject/PrimitiyProjectApplication.kt
@@ -0,0 +1,11 @@
+package me.hajoo.primitiyproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrimitiyProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrimitiyProjectApplication>(*args)
+}
